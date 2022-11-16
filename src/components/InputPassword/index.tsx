860f885984c9006dda1ce022154cc157732ae00e
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-16 17:28:59 -0300

    criação do input para mostrar e esconder senha

---
+++ src/components/InputPassword/index.tsx
@@ -0,0 +1,27 @@
+import Input from '../Input'
+import icon from '../../assets/Input/icon/icon.svg'
+import React, { useState } from 'react'
+
+const InputPassword = (): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <Input
+      type={showPassword ? 'text' : 'password'}
+      name="password"
+      placeholder="****************"
+    >
+      <div
+        className="
+          absolute right-0 cursor-pointer 
+          items-center justify-end h-9 flex 
+          w-10"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        <img src={icon} alt="icon" className="mr-2" />
+      </div>
+    </Input>
+  )
+}
+
+export default InputPassword
