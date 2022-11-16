0547aa900083f200c4a4c85b0ca47e435352ba3a
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-16 17:25:54 -0300

    mudança no input, nível de abstração reduzida

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,28 +1,22 @@
 import React from 'react'
-import icon from '../../assets/Input/icon/icon.svg'
 
 interface Props {
   placeholder: string
   type: string
   name: string
+  children?: React.ReactNode
 }
 
-const Input = ({ type, placeholder, name }: Props): JSX.Element => {
+const Input = ({ type, placeholder, name, children }: Props): JSX.Element => {
   return (
     <div className="w-full relative flex">
       <input
         type={type}
         name={name}
-        className="h-9 rounded-md text-[#535353] placeholder:text-[#535353] pl-2 w-full"
+        className="h-9 rounded-md text-[#535353] placeholder:text-[#535353] pl-2 w-full outline-none"
         placeholder={placeholder}
       />
-      {type === 'password' ? (
-        <div className="absolute items-center justify-end h-9 flex w-full">
-          <img src={icon} alt="" className="mr-2" />
-        </div>
-      ) : (
-        ''
-      )}
+      {children}
     </div>
   )
 }
