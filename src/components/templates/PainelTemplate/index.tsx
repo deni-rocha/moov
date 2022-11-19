80093ce72afa2119178fa379cbe0cb328c6dc21e
nosli <eu.denilsonrocha@gmail.com>
2022-11-19 18:00:50 -0300

    rafatoração dos componentes do Painel

--- src/components/templates/PainelTemplate/index.tsx
+++ src/components/templates/PainelTemplate/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import PainelContent from '../../organisms/painel/PainelContent'
+import PainelHeader from '../../organisms/painel/PainelHeader'
 import PainelSidebar from '../../organisms/painel/PainelSidebar'
+import PainelSearch from '../../organisms/painel/PainelSearch'
+import PainelData from '../../organisms/painel/PainelData'
 
 const PainelTemplate = (): JSX.Element => {
   return (
     <div className="w-11/12 max-w-[1181px] py-4 flex">
       <PainelSidebar />
-      <PainelContent />
+      <section className="grow p-2 flex flex-col">
+        <PainelHeader
+          subTittle="Controle de viagens"
+          tittle="Painel de controle de viagens"
+        />
+        <div className=" w-full pl-10 pt-4">
+          <PainelSearch />
+          <PainelData />
+        </div>
+      </section>
     </div>
   )
 }
