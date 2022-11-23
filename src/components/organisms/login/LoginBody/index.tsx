346456451f0ad01e6b30f21f15f1af3afb5f752f
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-23 16:35:42 -0300

    adiciona comentário informando qual é o componente reponsável por fazer o submit dos dados e logar na aplicação

--- src/components/organisms/login/LoginBody/index.tsx
+++ src/components/organisms/login/LoginBody/index.tsx
@@ -7,6 +7,7 @@ const LoginBody = (): JSX.Element => {
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
 
+  // o componente LoginBodyFooter é quem faz o submit dos dados
   return (
     <section className="flex flex-col gap-6 w-10/12 mt-3">
       <LoginBodyHeader email={email} setEmail={setEmail} />
