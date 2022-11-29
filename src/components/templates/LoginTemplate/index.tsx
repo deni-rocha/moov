3bdaf8628bd67e3fc6409e05ef97e292a0179abb
Denilson Rocha <eu.denilsonrocha@gmail.com>
2022-11-29 10:44:01 -0300

    importa o provider do loader para que os componentes filhos renderizem a cada atualização

--- src/components/templates/LoginTemplate/index.tsx
+++ src/components/templates/LoginTemplate/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import LoginBody from '../../organisms/login/LoginBody'
 import LoginHeader from '../../molecules/login/LoginHeader'
+import { LoaderProvider } from '../../../contexts/loader'
 
 const LoginTemplate = (): JSX.Element => {
   return (
-    <div
-      className="
+    <LoaderProvider>
+      <div
+        className="
         xs:w-10/12 xs:h-10/12
         bg-[#848484] text-white w-[400px] h-[517px] flex items-center flex-col rounded-xl drop-shadow-md
         "
-    >
-      <LoginHeader />
-      <LoginBody />
-    </div>
+      >
+        <LoginHeader />
+        <LoginBody />
+      </div>
+    </LoaderProvider>
   )
 }
 
